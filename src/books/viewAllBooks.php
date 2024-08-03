a7a6522fc35756bd640a3fadb642443953fddfe0
Disha Kolapate <83020695+DishaKolapte@users.noreply.github.com>
2024-08-03 06:58:46 +0530

    Update viewAllBooks.php
    In order to enable the functionality for updateBook, deleteBook, and addBook, I had to make modifications to this section of the code.

--- src/books/viewAllBooks.php
+++ src/books/viewAllBooks.php
@@ -4,8 +4,8 @@
     include __DIR__ . '/../reusable/utilities.php';
 
     $sql = "SELECT books.book_id, books.name AS book_name, authors.author_id, authors.author_name 
-    FROM books 
-    JOIN authors ON books.author_id = authors.author_id";
+            FROM books 
+            JOIN authors ON books.author_id = authors.author_id";
     $result = $connect->query($sql);
 ?>
 
@@ -15,17 +15,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="../..//css/styles.css">
+    <link rel="stylesheet" href="../../css/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100;400;700;900&display=swap" rel="stylesheet">
-    <title>Books & Movies CMS</title>
+    <title>Books List</title>
 </head>
 <body>
     <div class="container mt-5">
         <div class="row">
             <div class="col">
-            <?php
-                getMessage();
-            ?>
+                <?php getMessage(); ?>
             </div>
         </div>
         <div class="row mb-3">
@@ -41,9 +39,12 @@
                 <div class="col-md-4">
                     <div class="card mb-4">
                         <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['book_name']; ?></h5>
+                            <h5 class="card-title"><?php echo htmlspecialchars($row['book_name']); ?></h5>
                             <p class="card-text">
-                                <a class="author-link" href="authorDetails.php?id=<?php echo $row['author_id']; ?>"><?php echo $row['author_name']; ?></a>
+                                <strong>Author:</strong> 
+                                <a href="authorDetails.php?id=<?php echo $row['author_id']; ?>">
+                                    <?php echo htmlspecialchars($row['author_name']); ?>
+                                </a>
                             </p>
                             <a href="bookDetails.php?id=<?php echo $row['book_id']; ?>" class="btn btn-primary">View Details</a>
                         </div>
